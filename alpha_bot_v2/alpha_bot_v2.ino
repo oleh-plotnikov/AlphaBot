c3b57908580d82b637a6acb8ffa5d3157dd67703
Oleh Plotnikov <oleh.plotnikov90@gmail.com>
2019-02-02 13:40:42 +0200

    fixed project compilation

--- alpha_bot_v2/alpha_bot_v2.ino
+++ alpha_bot_v2/alpha_bot_v2.ino
@@ -1,57 +1,6 @@
-#include <TRSensors.h>          //< Line sensors driver
-#include <Wire.h>               //< IO extender driver
-
-// Motor definitions
-#define PWMA   (6)   //Left Motor Speed pin (ENA)
-#define AIN2   (A0)  //Motor-L forward (IN2).
-#define AIN1   (A1)  //Motor-L backward (IN1)
-#define PWMB   (5)   //Right Motor Speed pin (ENB)
-#define BIN1   (A2)  //Motor-R forward (IN3)
-#define BIN2   (A3)  //Motor-R backward (IN4)
-
-// Line sensors definitions
-#define NUM_SENSORS   (5)
-
-// IO definitions
-#define IO_I2C_ADDR        (0x20)
-#define IO_MASK_BEEP_ON    (0xDF)
-#define IO_MASK_BEEP_OFF   (0x20)
-#define IO_MASK_JOY_UP     (0xFE)
-#define IO_MASK_JOY_DOWN   (0xF7)
-#define IO_MASK_JOY_LEFT   (0xFB)
-#define IO_MASK_JOY_RIGHT  (0xFD)
-#define IO_MASK_JOY_CENTER (0xEF)
-#define IO_MASK_DEFAULT    (0xE0)  //< Beep off, joystick released
-#define IO_MASK_RESET      (0xFF)  //< Reset IO state
-
-// Types declaration
-/**
- * @brief PID controller description
- */
-typedef struct PID_descr
-{
-  const  int K_P;    // proportional gain
-  const  int K_I;    // integral gain
-  const  int K_D;    // derivative gain
-  const  int OUT_MAX;  // maximal output
-  long      integral; // integral value
-  int       last_err; // last error value
-} PID_descr_t;
-
-/**
- * @brief Robot states
- */
-typedef enum
-{
-  STATE_SETUP,
-  STATE_CALIBRATION,
-  STATE_WAIT_START,
-  STATE_GO,
-  STATE_STOP,
-  STATE_MAKE_DECISION,
-  STATE_FINISH
-} Robot_state_t;
-
+#include <TRSensors.h>
+#include <Wire.h>
+#include "alpha_bot_v2.h"
 
 TRSensors trs = TRSensors();
 unsigned int line_sensor_buffer[NUM_SENSORS];
@@ -65,7 +14,7 @@ unsigned char select_turn(unsigned char found_left, unsigned char found_straight
 void SetSpeeds(int left,int right);
 void PCF8574Write(byte data);
 byte PCF8574Read();
-bool is_button_pressed(byte button_mask);
+bool is_button_pressed(Command_type_t command);
 void beep(unsigned long duration_ms);
 
 // Loop functions
@@ -85,7 +34,7 @@ void setup()
   pinMode(AIN2,OUTPUT);  
   SetSpeeds(0,0);
 
-  while(!is_button_pressed(IO_MASK_JOY_CENTER))
+  while(!is_button_pressed(eCOMMAND_START))
   {
     delay(10);
   }
@@ -96,23 +45,7 @@ void setup()
 
 int wait_command(const char* str)
 {
-  String comdata = "";
-  while (Serial.available() > 0)  
-  {
-    comdata += char(Serial.read());
-    delay(2);
-  }
-  if (comdata.length() > 0)
-  {
-    Serial.println(comdata);
-    const char* command = comdata.c_str();
-    if(strcmp(command, str) == 0)          //Forward
-    {
-      return 1;
-    }
-  }
-  comdata = "";
-  return 0;
+  return true;
 }
 
 // Main loop
@@ -138,7 +71,7 @@ void loop()
     }
     case STATE_WAIT_START:
     {
-      if (is_button_pressed(IO_MASK_JOY_CENTER))
+      if (is_button_pressed(eCOMMAND_START))
       {
         // Reset current PID state and start to move
         PID_control(&PID_gear, 0, 0, true);
@@ -402,12 +335,57 @@ byte PCF8574Read()
   return static_cast<byte>(data);
 }
 
-bool is_button_pressed(byte button_mask)
+bool is_button_pressed(Command_type_t command)
 {
+  byte button_mask = IO_MASK_JOY_CENTER;
+  switch (command)
+  {
+    case eCOMMAND_START:
+      button_mask = IO_MASK_JOY_CENTER;
+      break; 
+/*
+    case COMMAND_:
+      byte button_mask = IO_MASK_JOY_DOWN;
+      break; 
+    case COMMAND_:
+      byte button_mask = IO_MASK_JOY_UP;
+      break; 
+    case COMMAND_:
+      byte button_mask = IO_MASK_JOY_LEFT;
+      break; 
+    case COMMAND_:
+      byte button_mask = IO_MASK_JOY_RIGHT;
+      break;
+*/
+    default:
+      button_mask = IO_MASK_JOY_CENTER;
+      break;
+  }
+  
+#if DEBUG_MODE
   byte reg_value = IO_MASK_RESET;
   PCF8574Write(reg_value);
   reg_value = PCF8574Read() | IO_MASK_DEFAULT;
   return reg_value == button_mask;
+#else
+  String comdata = "";
+  while (Serial.available() > 0)  
+  {
+    comdata += char(Serial.read());
+    delay(2);
+  }
+  if (comdata.length() > 0)
+  {
+    Serial.println(comdata);
+    const char* command = comdata.c_str();
+    if(strcmp(command, str) == 0)          //Forward
+    {
+      return 1;
+    }
+  }
+  comdata = "";
+  return 0;
+#endif 
 }
 
 void beep(unsigned long duration_ms)
---
+++ test/test.ino
@@ -0,0 +1,13 @@
+#include <TRSensors.h>
+
+TRSensors trs = TRSensors();
+
+void setup()
+{
+
+}
+
+void loop()
+{
+
+}
